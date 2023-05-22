835d56820edb9fd3b9fdf6cc861a055305c8cf03
John <kimo87@gmail.com>
2023-05-22 19:22:45 +0900

    Command 추가
    Command 추가

--- WPF_STUDY/WPF_STUDY/TestClickCommand.cs
+++ WPF_STUDY/WPF_STUDY/TestClickCommand.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using WPF_STUDY.ViewModels;
 
 namespace WPF_STUDY
 {
@@ -13,16 +14,29 @@ namespace WPF_STUDY
     /// </summary>
     class TestClickCommand : ICommand
     {
+        private bool rtnCan = true;
+
+        private MainVIewModel _mainVIewModel;
+
+
         public event EventHandler? CanExecuteChanged;
 
-        public bool CanExecute(object? parameter)
+        public TestClickCommand( MainVIewModel mainVIewModel)
         {
-            return true;
+            _mainVIewModel = mainVIewModel;
+        }
+
+            public bool CanExecute(object? parameter)
+        {
+
+            return rtnCan;
         }
 
         public void Execute(object? parameter)
         {
-            MessageBox.Show("aaa");
+            rtnCan = false;
+
+            MessageBox.Show(_mainVIewModel.ProgressValue+"");
         }
     }
 }
--- WPF_STUDY/WPF_STUDY/ViewModels/MainVIewModel.cs
+++ WPF_STUDY/WPF_STUDY/ViewModels/MainVIewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace WPF_STUDY.ViewModels
@@ -16,20 +17,36 @@ namespace WPF_STUDY.ViewModels
 
         public MainVIewModel()
         {
-            TestClick  =    new    TestClickCommand(); 
+            TestClick = new RelayCommand<object>(ExecuteMyButton,CanMyButton);
 
         }
         public int ProgressValue
         {
             get { return progressValue; }
-            set { progressValue = value; 
+            set
+            {
+                progressValue = value;
                 NotifyPropertyChanged(nameof(ProgressValue));
             }
         }
 
+         
+        bool CanMyButton(object param)
+        {
+            if(param == null)
+            {
+                return true ;   
+            }
+            return param.ToString().Equals("ABC") ? true : false; 
+        }
+
+        void ExecuteMyButton(object param)
+        {
+            MessageBox.Show(param.ToString()+"AAA");
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void NotifyPropertyChanged([CallerMemberName] String PropertyName ="")
+        private void NotifyPropertyChanged([CallerMemberName] String PropertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
