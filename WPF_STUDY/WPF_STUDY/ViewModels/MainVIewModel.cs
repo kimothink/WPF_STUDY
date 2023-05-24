04436ae1e70bef4580dfeec46b3983690cecc41f
John <kimo87@gmail.com>
2023-05-24 17:03:32 +0900

    유저 컨트롤 추가
    유저 컨트롤 추가

--- WPF_STUDY/WPF_STUDY/ViewModels/MainVIewModel.cs
+++ WPF_STUDY/WPF_STUDY/ViewModels/MainVIewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -17,8 +19,8 @@ namespace WPF_STUDY.ViewModels
 
         public MainVIewModel()
         {
-            TestClick = new RelayCommand<object>(ExecuteMyButton,CanMyButton);
-
+            //TestClick = new RelayCommand<object>(ExecuteMyButton,CanMyButton);
+            TestClick = new AsyncRelayCommand(ExecuteMyButton2);
         }
         public int ProgressValue
         {
@@ -42,10 +44,51 @@ namespace WPF_STUDY.ViewModels
 
         void ExecuteMyButton(object param)
         {
-            MessageBox.Show(param.ToString()+"AAA");
+            //MessageBox.Show(param.ToString()+"AAA");
+            int w = 0;
+            Task task1 = Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    ProgressValue = i;
+                }
+            });
+
+            Task rtn2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    ProgressValue = i;
+                    w = i;
+                    Thread.Sleep(2000);
+                }
+            });
+
+            rtn2.Wait();
+            MessageBox.Show(w + "");
         }
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public async Task ExecuteMyButton2()
+        {
+            int w = 0;
+            Task<int> rtn2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    ProgressValue = i;
+                    w = i;
+                    Thread.Sleep(2000);
+                }
+                int j = 5;
+                return j;
+            });
+
+            w = await rtn2;
+
+            MessageBox.Show(w + "");
+
+        }
         private void NotifyPropertyChanged([CallerMemberName] String PropertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
